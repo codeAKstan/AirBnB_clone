fbb8cc1c6f2d156a0f6d437bda30c9293800c4ea
kmmadu <mmadubugwukingsley@gmail.com>
2024-02-10 17:48:40 +0100

    base_model.py file

--- models/base_model.py
+++ models/base_model.py
@@ -8,17 +8,31 @@ import uuid
 
 class BaseModel:
     """ the base model for other classes """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+
         self.updated_at = datetime.now()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at' or key == 'updated_at':
+                    datetime_value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, datetime_value)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+
+
 
     def __str__(self):
         """ string representation """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """ save methof that updates the updated_at """
+        """ save method that updates the updated_at """
         self.updated_at = datetime.now()
 
     def to_dict(self):
